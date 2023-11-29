823649cf84b6e8fb139869f2eacecd631fb4d345
Бакытбек <bakytbek@MacBook-Pro-Bakytbek.local>
2023-11-30 02:53:13 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { urlencoded, json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(json({ limit: '50mb' }));
+  app.use(json({ limit: '100mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT);
 }
